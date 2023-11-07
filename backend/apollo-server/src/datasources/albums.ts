c44761b0c9599e2f9c547f44af2fa0ba6f85fa94
ybabenkoJapp <ybabenko@jappware.com>
2023-11-07 10:08:26 +0200

    feat(albums.ts); create encapsulated class for data fetching

---
+++ backend/apollo-server/src/datasources/albums.ts
@@ -0,0 +1,17 @@
+import { RESTDataSource } from '@apollo/datasource-rest';
+
+export class Albums extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = 'https://jsonplaceholder.typicode.com/';
+    }
+
+    async getAlbums() {
+        return await this.get('albums')
+    }
+
+    async getAlbumById(id: number) {
+        return await this.get(`albums${id}`)
+    }
+
+}
